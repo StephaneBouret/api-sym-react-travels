a436590aa3a3f17816cc57e022e12bf6b7bbeacd
Stéphane BOURET <sbouret@orange.fr>
2022-03-30 14:41:49 +0200

    Mise en place du Slider

---
+++ assets/js/components/slider/Slider.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import Carousel from "react-bootstrap/Carousel";
+import "./Slider.css";
+import "./animate.css";
+
+const Slider = () => {
+    return ( 
+        <Carousel className="slick-slide slick-slider--full-height">
+            <Carousel.Item>
+                <img 
+                className="d-block w-100 slider-login"
+                src={require('../../../media/slider-1.jpg')} 
+                alt="First slide" 
+                />
+                <Carousel.Caption>
+                    <div className="slide-text">
+                        <h2 className="animate__animated animate__fadeInDown"><span>Luxury</span> Travel</h2>
+                        <p className="animate__animated animate__fadeInUp">Spécialiste des voyages de luxe et des voyages sur mesure</p>
+                    </div>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img 
+                className="d-block w-100 slider-login"
+                src={require('../../../media/slider-2.jpg')} 
+                alt="Second slide" 
+                />
+                <Carousel.Caption>
+                    <div className="slide-text">
+                        <h2 className="animate__animated animate__fadeInDown"><span>Luxury</span> Travel</h2>
+                        <p className="animate__animated animate__fadeInUp">L'ouverture des pays se poursuit ! Où et quand partir ?</p>
+                    </div>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img 
+                className="d-block w-100 slider-login"
+                src={require('../../../media/slider-3.jpg')} 
+                alt="Third slide" 
+                />
+                <Carousel.Caption>
+                    <div className="slide-text">
+                        <h2 className="animate__animated animate__fadeInDown"><span>Luxury</span> Travel</h2>
+                        <p className="animate__animated animate__fadeInUp">Un voyage, une rencontre</p>
+                    </div>
+                </Carousel.Caption>
+            </Carousel.Item>
+        </Carousel>
+     );
+}
+ 
+export default Slider;
--- assets/js/pages/homePage/HomePage.jsx
+++ assets/js/pages/homePage/HomePage.jsx
@@ -1,14 +1,19 @@
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import React from 'react';
+import Slider from '../../components/slider/Slider';
 import "./HomePage.css";
 
 const HomePage = () => {
+    AOS.init({
+        duration: 1000
+    });
+    
     return ( 
         <>
         <section id="hero">
             <div className="hero-container">
-
+                <Slider/>
             </div>
         </section>
         </>
